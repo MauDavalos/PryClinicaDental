69f988b804f3ea538e1036b2b011ca244f328e6c
MauDavalos <madvalosj@espe.edu.ec>
2019-11-29 10:37:05 -0500

    security

---
+++ src/main/java/com/dental/app/web/SpringSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.dental.app.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.dental.app.web.security.LoginSuccessHandler;
+
+@EnableGlobalMethodSecurity(securedEnabled=true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	@Autowired //crear un objeto de esta clase dentro de la clase de arriba
+	private LoginSuccessHandler successHandler;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+		
+	}
+	
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
+		
+		PasswordEncoder encoder = passwordEncoder();
+		UserBuilder users = User.builder().passwordEncoder(password -> encoder.encode(password));
+		builder.inMemoryAuthentication()
+		.withUser(users.username("admin1").password("123").roles("ADMIN"))
+		.withUser(users.username("hernansiqui").password("123").roles("USER"))
+		.withUser(users.username("mau").password("123").roles("USER"));
+	}
+	
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/","/css/**","/js/**","images/**").permitAll()
+		.antMatchers("/paciente/**").hasAnyRole("ADMIN","USER")
+		.antMatchers("/doctor/**").hasAnyRole("ADMIN")
+		.antMatchers("/insumo/**").hasAnyRole("ADMIN")
+		.antMatchers("/prescripcion/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+			.formLogin().successHandler(successHandler)
+			.loginPage("/login").permitAll()
+		.and()
+			.logout().permitAll()
+		.and()
+			.exceptionHandling().accessDeniedPage("/error_403");
+	}*/
+
+}
--- src/main/java/com/dental/app/web/controllers/HomeController.java
+++ src/main/java/com/dental/app/web/controllers/HomeController.java
@@ -1,9 +1,13 @@
 package com.dental.app.web.controllers;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value="/")
@@ -14,6 +18,22 @@ public class HomeController {
 		model.addAttribute("title", "Dental Pro ®");
 		model.addAttribute("description", "Sistema de Clínica Dental");
 		return "home";
+	}
+	
+	@GetMapping(value="/login")
+	public String login(@RequestParam(value="error", required=false) String error
+			, Model model, Principal principal, RedirectAttributes flash) {
+		
+		if(principal!= null) {
+			flash.addFlashAttribute("info", "El usuario tiene una sesión activa");
+			return "redirect:/";
+		}
+		
+		if(error!= null) {
+			model.addAttribute("error", "Usuario o contrasena incorrectas");
+		}
+		
+		return "login";
 	}
 	
 
